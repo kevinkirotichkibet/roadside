1fbb24c8d16f60448b9cf5f23c78028003cd7841
Kevin Kirotich Kibet <kirotichkevin@gmail.com>
2023-05-02 17:09:20 +0300

    EOD

--- roadside/App.js
+++ roadside/App.js
@@ -5,6 +5,9 @@ import { StyleSheet, Text, View, Dimensions } from 'react-native';
 // import { OriginContextProvider, DestinationContextProvider } from './src/contexts/contexts';
 // import RoootNavigator from './src/navigations/RootNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { colors } from 'react-native-elements';
+import { parameters } from './src/global/styles';
+import { ScreenWidth } from 'react-native-elements/dist/helpers';
 const App = () => {
   return (
     <NavigationContainer>
@@ -21,7 +24,94 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.white,
+    paddingBottom: 30,
   },
+  header: {
+    backgroundColor: colors.blue,
+    paddingTop: parameters.statusBarHeight,
+    height: parameters.headerHeight,
+    alignItems: 'flex-start',
+  },
+  image1: {
+    height: 100,
+    width: 100,
+  },
+  image2: { height: 100, width: 100, borderRadius: 30 },
+  home: { backgroundColor: colors.blue, paddingLeft: 20 },
+  text1: { color: colors.white, fontSize: 20, paddingBottom: 20, paddingTop: 20 },
+  text2: { color: colors.white, fontSize: 16 },
+  view1: { flexDirection: row, flex: 1, paddingTop: 30 },
+  button1: {
+    height: 40,
+    width: 150,
+    backgroundColor: colors.black,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  button1Text: { color: colors.white, fontSize: 17, marginTop: -2 },
+  card: { alignItems: 'center', margin: ScreenWidth / 22 },
+  view2: { marginBottom: 5, borderRadius: 20, backgroundColor: colors.grey5 },
+  title: { color: colors.black, fontSize: 16 },
+  view3: {
+    flexDirection: 'row',
+    marginTop: 5,
+    backgroundColor: colors.grey5,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 15,
+  },
+  text3: { marginLeft: 20, fontSize: 15, color: colors.black },
+  view4: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 15,
+    backgroundColor: colors.white,
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+    borderRadius: 20,
+  },
+  view5: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingVertical: 25,
+    justifyContent: 'space-between',
+    marginHorizontal: 15,
+    borderBottomColor: colors.grey4,
+    borderBottomWidth: 1,
+    flex: 1,
+  },
+  view6: {
+    alignItems: 'center',
+    flex: 5,
+    flexDirection: 'row',
+  },
+  view7: {
+    backgroundColor: colors.grey5,
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 20,
+  },
+  map: { height: 150, width: ScreenWidth * 0.92 },
+  text4: { fontSize: 20, color: colors.black, marginLeft: 20, marginBottom: 20 },
+  icon1: { marginLeft: 10, marginTop: 5 },
+  view8: { flex: 4, marginTop: -25 },
+  carsAround: { width: 28, height: 14 },
+  location: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: colors.blue,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view9: { width: 4, height: 4, borderRadius: 2, backgroundColor: colors.white },
 });
 
 // import React, { useState } from 'react';
